f007d5e3257c68ff56fda3b1ad9072de76ac8315
Jen Nguyen <26128269+ItsJenOClock@users.noreply.github.com>
2025-02-09 23:21:37 -0600

    updated crab count

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,17 @@
-const addCrab = () => {
+const state = {
+  crabCount: 0
+};
+
+const addCrab = (event) => {
   const newCrab = document.createElement("span");
   const crabContainer = document.querySelector("#crabContainer");
   newCrab.textContent = "🦀";
   crabContainer.appendChild(newCrab);
+
+  state.crabCount += 1;
+  const crabCountCounter = document.querySelector("#crabCount");
+  crabCountCounter.textContent = `Crab Count: ${state.crabCount}`;
+
 };
 
 const registerEventHandlers = () => {
